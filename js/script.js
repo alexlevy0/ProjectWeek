08d0ee613f20cdc38ac8b3104f12f09df7d98d6c
rivory <geoffrey.rivory@epitech.eu>
2016-03-10 17:22:09 +0100

    couleur a la charte

--- js/script.js
+++ js/script.js
@@ -41,9 +41,9 @@ $(document).ready(
   function() { 
 
     $("html").niceScroll({
-    	cursorcolor:"#f74d65",
+    	cursorcolor:"#8BD19B",
     	scrollspeed :"100",
-    	cursorborder:"1px solid #f74d65",
+    	cursorborder:"1px solid #8BD19B",
     	horizrailenabled: "false",
     	cursorborderradius: "0px"
     });
